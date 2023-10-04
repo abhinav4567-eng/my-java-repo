98a2488980777225766312ec9606b0a7f37485e1
Abhinav Singh <abhinavadarshsingh007@gmail.com>
2023-10-04 12:47:52 +0530

    diff is added

--- Bodmas.java
+++ Bodmas.java
@@ -1,9 +1,11 @@
 class Bodmas {
     public static void main(String[] args) {
      
-       int a=5;
+       int a=12;
        int b=10;
        int c=a+b;
+       int d=a-b;
        System.out.println("Sum is "+c);
+       System.out.println("multiplication is "+d);
     }
 }
